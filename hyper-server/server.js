b218e37e4a53f3291e1e1f48667e9403af94faaf
陈言 <chenyan@s-MacBook-Pro.phub.net.cable.rogers.com>
2021-06-08 22:18:07 -0400

    share

--- hyper-server/server.js
+++ hyper-server/server.js
@@ -3,36 +3,32 @@ const port = 3000;
 var express = require("express"),
   app = express();
 
+var data = [];
+
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
-var RESULT_FOLDER = "result";
-
 app.use(express.json());
 
-app.get("/11", function (req, res) {
-  //   res.render("form"); // if jade
-  //   // You should use one of line depending on type of frontend you are with
-  //   res.sendFile(__dirname + "/form.html"); //if html file is root directory
-  //   res.sendFile("index.html"); //if html file is within public directory
-
-  console.log(req.body);
-  //   console.log(username);
-});
-
 app.post("/1", function (req, res) {
-  //   var username = req.body.username;
-  //   var htmlData = "Hello:" + username;
-  //   res.send(htmlData);
-
-  console.log(req.body);
-  fs.writeFile("test.txt", JSON.stringify(req.body), (err) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
+  var id = new Date().getTime();
+  var userData = JSON.stringify(req.body);
+  data.push({
+    id: id,
+    data: userData,
   });
+  console.log(data);
+
+  // fs.writeFile("test.txt", JSON.stringify(req.body), (err) => {
+  //   if (err) throw err;
+  //   console.log("The file has been saved!");
+  //   return;
+  // });
+
+  // res.send("ok");
 });
 
 app.listen(port);
